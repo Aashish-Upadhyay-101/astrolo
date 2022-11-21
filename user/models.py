39cd1fa29df8f8cdc8a4c096b98a6bf933140b5f
Aashish-Upadhyay-101 <boysmall300@gmail.com>
2022-11-21 22:14:52 +0545

    feat: user model

--- user/models.py
+++ user/models.py
@@ -1,3 +1,37 @@
+import uuid
 from django.db import models
+from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin
+from django.utils import timezone
+from .manager import UserManager
 
-# Create your models here.
+
+class User(AbstractBaseUser, PermissionsMixin):
+    pkid = models.BigAutoField(primary_key=True, editable=False)
+    id = models.UUIDField(unique=True, default=uuid.uuid4, editable=False)
+    username = models.CharField(max_length=50, unique=True)
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    email = models.EmailField(max_length=120, unique=True)
+    is_active = models.BooleanField(default=False)
+    is_staff = models.BooleanField(default=False)
+    date_joined = models.DateTimeField(default=timezone.now)
+
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = ["username", "first_name", "last_name", "email"]
+
+    objects = UserManager()
+
+
+    class Meta:
+        verbose_name = "User"
+        verbose_name_plural = "Users"
+
+    
+    def __str__(self):
+        return self.username 
+
+    @property
+    def get_full_name(self):
+        return f"{self.first_name} {self.last_name}"
+
+
