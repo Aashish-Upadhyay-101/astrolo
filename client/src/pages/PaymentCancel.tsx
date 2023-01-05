2548d7a4db60c13e08298f29b043558e8254e272
Aashish-Upadhyay-101 <boysmall300@gmail.com>
2023-01-05 20:43:48 +0545

    success / cancel page

--- client/src/pages/PaymentCancel.tsx
+++ client/src/pages/PaymentCancel.tsx
@@ -1,7 +1,21 @@
+import { Result } from "antd";
 import React from "react";
+import { Link } from "react-router-dom";
 
 const PaymentCancel = () => {
-  return <div>PaymentCancell</div>;
+  return (
+    <Result
+      style={{ marginTop: "8.2rem" }}
+      status="error"
+      title="Submission Failed"
+      subTitle="Please check and modify the following information before resubmitting."
+      extra={[
+        <Link to="/astrolo" className="btn btn-primary">
+          Retry
+        </Link>,
+      ]}
+    />
+  );
 };
 
 export default PaymentCancel;
--- client/src/pages/PaymentSuccess.tsx
+++ client/src/pages/PaymentSuccess.tsx
@@ -1,7 +1,21 @@
+import { Button, Result } from "antd";
 import React from "react";
+import { Link } from "react-router-dom";
 
 const PaymentSuccess = () => {
-  return <div>PaymentSuccess</div>;
+  return (
+    <Result
+      style={{ marginTop: "8.2rem" }}
+      status="success"
+      title="Successfully Purchased Cloud Server ECS!"
+      subTitle="Order number: 2017182818828182881 Cloud server configuration takes 1-5 minutes, please wait."
+      extra={[
+        <Link to="/dashboard" className="btn btn-primary">
+          Goto Dashboard
+        </Link>,
+      ]}
+    />
+  );
 };
 
 export default PaymentSuccess;
