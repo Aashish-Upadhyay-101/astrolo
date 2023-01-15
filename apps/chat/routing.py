b396b9743c0ec9153b4bdbc97f2cf74c46232afe
Aashish-Upadhyay-101 <boysmall300@gmail.com>
2023-01-15 11:28:30 +0545

    feat: basic chat consumer setup

---
+++ apps/chat/routing.py
@@ -0,0 +1,10 @@
+from django.urls import re_path
+
+from . import consumers
+
+
+websocket_urlpatterns = [
+
+]
+
+
--- astrolo/asgi.py
+++ astrolo/asgi.py
@@ -8,14 +8,23 @@ https://docs.djangoproject.com/en/4.1/howto/deployment/asgi/
 """
 
 import os
+from pathlib import Path
+import sys 
 
 from django.core.asgi import get_asgi_application
-from channels.routing import ProtocolTypeRouter
+from channels.routing import ProtocolTypeRouter, URLRouter
+from apps.chat.routing import websocket_urlpatterns
+
+ROOT_DIR = Path(__file__).resolve(strict=True).parent.parent
+sys.path.append(str(ROOT_DIR / "chat"))
+
 
 django_asgi_app = get_asgi_application()
+
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'astrolo.settings')
 
 application = ProtocolTypeRouter({
     "http": django_asgi_app,
+    "websocket": URLRouter(websocket_urlpatterns),
 })
